0063bfcd297a4853edbdbadc709ec5222efed057
Ryan MacKenzie <rienmack@gmail.com>
2022-12-01 08:50:15 -0500

    first star completed

---
+++ 01-12-2022/december-1st-pt-1/target/debug/december-1st.d
@@ -0,0 +1,1 @@
+/Users/ryanmackenzie/advent-of-code/01-12-2022/december-1st-pt-1/target/debug/december-1st: /Users/ryanmackenzie/advent-of-code/01-12-2022/december-1st-pt-1/src/main.rs
---
+++ 01-12-2022/december-1st-pt-1/target/debug/deps/december_1st-80e5ac408e8751fc.d
@@ -0,0 +1,5 @@
+/Users/ryanmackenzie/advent-of-code/01-12-2022/december-1st-pt-1/target/debug/deps/december_1st-80e5ac408e8751fc.rmeta: src/main.rs
+
+/Users/ryanmackenzie/advent-of-code/01-12-2022/december-1st-pt-1/target/debug/deps/december_1st-80e5ac408e8751fc.d: src/main.rs
+
+src/main.rs:
